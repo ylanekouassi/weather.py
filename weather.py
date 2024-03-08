3f7e415c90b8c11a9e05178bd1fcd894b2945e48
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-03-07 22:59:32 -0500

    module config

--- weather.py
+++ weather.py
@@ -2,3 +2,11 @@ import requests
 
 api_key ="dec97d8ebbe5dae57315a0b61d3abbda"
 api_url = "https://api.openweathermap.org/data/2.5/weather" #?q={city name}&appid={API key} removed = weather now
+
+response = requests.get(
+    url=api_url,
+    params={
+        "q": "Ottawa",
+        "appid": api_key,
+                }
+)
