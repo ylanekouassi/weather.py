f72493eddfe49848052d60766b9e02cee7131d1b
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-03-07 23:27:03 -0500

    input created, and params updated

--- weather.py
+++ weather.py
@@ -3,13 +3,15 @@ import requests
 api_key ="dec97d8ebbe5dae57315a0b61d3abbda"
 api_url = "https://api.openweathermap.org/data/2.5/weather" #?q={city name}&appid={API key} removed = weather now
 
+city = input("Enter the city: ")
+
 response = requests.get(
     url=api_url,
     params={
-        "q": "Ottawa",
+        "q": city,
         "appid": api_key,
                 }
 )
 
 weather = response.json()
-print(weather)
+print(weather["main"]["temp"])
