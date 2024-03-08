6aac44cbdbc2e36c1f7e85ea436c2a614d915442
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-03-07 22:52:31 -0500

    module required imported

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
-api_key ="dec97d8ebbe5dae57315a0b61d3abbda"
+import requests
 
-"https://api.openweathermap.org/data/2.5/weather" #?q={city name}&appid={API key} removed
+api_key ="dec97d8ebbe5dae57315a0b61d3abbda"
+api_url = "https://api.openweathermap.org/data/2.5/weather" #?q={city name}&appid={API key} removed = weather now
